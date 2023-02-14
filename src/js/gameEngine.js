394b4270f12ce9ebb831d5f8222d7222f98fbdc2
Mihail Zaykov <mixail_zaikov@abv.bg>
2023-02-14 12:32:20 +0200

    Move to the right

--- src/js/gameEngine.js
+++ src/js/gameEngine.js
@@ -5,6 +5,16 @@ function start(state, game){
 }
 
 function gameLoop(state, game){
+  const { wizard } = state;
+  const { wizardElement } = game;
+
+
+  if(state.keys.KeyD){
+    console.log(state.keys);
+    wizard.posX += 10;
+  }
+
+  wizardElement.style.left = wizard.posX + 'px'; 
 
   window.requestAnimationFrame(gameLoop.bind(null, state, game));
 }
--- src/js/gameState.js
+++ src/js/gameState.js
@@ -1,11 +1,13 @@
 function initState() {
+  let startX =  Math.floor(Math.random() * 700);
+  let startY = Math.floor(Math.random() * 500);
   const state = {
     player: 'Pesho',
     wizard: {
       width: 82,
       height: 100,
-      startX: Math.floor(Math.random() * 700),
-      startY: Math.floor(Math.random() * 500),
+      posX: startX,
+      posY: startY,
     },
     keys: {}
   }
