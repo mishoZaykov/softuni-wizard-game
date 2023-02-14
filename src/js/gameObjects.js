44d7c25a6ab4efb612e171d5c8d1e658c8bae73b
Mihail Zaykov <mixail_zaikov@abv.bg>
2023-02-14 12:07:59 +0200

    Create wizard with initial position on start

--- src/js/gameObjects.js
+++ src/js/gameObjects.js
@@ -5,5 +5,20 @@ function initGameObjects(){
   return{
     startScreen,
     gameScreen,
+    createWizard(initialState){
+      let wizardElement = document.createElement('div');
+      wizardElement.classList.add('wizard');
+
+      wizardElement.style.width = initialState.width + 'px';
+      wizardElement.style.height = initialState.height + 'px';
+
+      wizardElement.style.left = initialState.startX + 'px';
+      wizardElement.style.top = initialState.startY + 'px';
+
+      this.wizardElement = wizardElement;
+      gameScreen.appendChild(wizardElement);
+
+      return wizard;
+    }
   };
 }
--- src/js/gameState.js
+++ src/js/gameState.js
@@ -1,6 +1,12 @@
 function initState() {
   const state = {
-    player: 'Pesho'
+    player: 'Pesho',
+    wizard: {
+      width: 82,
+      height: 100,
+      startX: Math.floor(Math.random() * 700),
+      startY: Math.floor(Math.random() * 500),
+    }
   }
 
   return state;
